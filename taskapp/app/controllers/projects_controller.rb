dfe69c8b23e6cd76e79104b92ae127892b1fc036
shule517 <shule517@gmail.com>
2016-12-03 01:10:52 +0900

    projectの新規追加

--- taskapp/app/controllers/projects_controller.rb
+++ taskapp/app/controllers/projects_controller.rb
@@ -6,4 +6,19 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path
+  end
+end
+
+private
+def project_params
+  params[:project].permit(:title)
 end
